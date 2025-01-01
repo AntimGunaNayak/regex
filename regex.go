df056800cdf6e5cba348bb80f838a381d1c33a2c
AntimGunaNayak <ulti9256@gmail.com>
2025-01-01 13:51:46 +0530

    adding a hello check for package

---
+++ regex.go
@@ -0,0 +1,10 @@
+package regex
+
+import(
+	"fmt"
+)
+
+func TestHello(){
+	// purpose of this is to check if external go file can call this module via the github path ( learning in progress - don't judge )
+	fmt.Println("hello world")
+}
